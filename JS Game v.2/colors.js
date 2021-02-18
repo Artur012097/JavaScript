8b984accc54b8255841112de71e1d9392872396d
artur012097 <artur012097.ax@gmail.com>
2021-02-18 15:33:53 +0400

    I've changed colors to constant.

--- JS Game v.2/colors.js
+++ JS Game v.2/colors.js
@@ -1,6 +1,6 @@
-let gray = '#ccc'
-let white = '#fff'
-let black = '#000'
+const white = '#fff'
+const black = '#000'
+const gray = '#ccc'
 let boxColors = [
     '#3252d1', '#1fff9a', '#ff4c4c', '#7a4444', '#ff03af', '#ba7ba6',
     '#14ffff', '#57ff14', '#60cc35', '#d9ff00', '#566117', 
