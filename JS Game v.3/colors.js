e2aca3b75b4c0f6fe6c0736b11b5c343942d187b
artur012097 <artur012097.ax@gmail.com>
2021-02-18 15:39:32 +0400

    I've added v.3 for this game to github to try write it after some days.

---
+++ JS Game v.3/colors.js
@@ -0,0 +1,7 @@
+const white = '#fff'
+const black = '#000'
+const gray = '#ccc'
+let boxBg = [
+    '#3252d1', '#1fff9a', '#ff4c4c', '#7a4444', '#ff03af', '#ba7ba6',
+    '#14ffff', '#57ff14', '#60cc35', '#d9ff00', '#566117', 
+]
