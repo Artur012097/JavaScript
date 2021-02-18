93bcc8c37f79933f5596e8a21f8b5f3bf7a990ce
artur012097 <artur012097.ax@gmail.com>
2021-02-18 14:28:08 +0400

    Дописал игру на с прасмотром видео.

--- JS Game/index.js
+++ JS Game/index.js
@@ -1,21 +1,64 @@
 let $game = document.querySelector('#game');
 let $start = document.querySelector('#start');
-let handelTime = document.querySelector('#time-header')
-let handleScore = document.querySelector('#result-header')
-let result = document.querySelector('#result')
-let isGameStarted = false
+let $handelTime = document.querySelector('#time-header')
+let $handleScore = document.querySelector('#result-header')
+let $time = document.querySelector('#time')
+let $gameTime = document.querySelector('#game-time')
+let $result = document.querySelector('#result')
 
 let score = 0
+let isGameStarted = false
 
-$start.addEventListener('click', gameStart);
+$start.addEventListener('click', gameStart)
+document.addEventListener('click', handleBoxClick)
+$gameTime.addEventListener('input', setGameTime)
 
 function gameStart() {
+    score = 0
+    isGameStarted = true
+    setGameTime()
+    $handelTime.classList.remove('hide')
+    $handleScore.classList.add('hide')
     $game.style.backgroundColor = '#fff'
     $start.classList.add('hide')
+    $gameTime.setAttribute('disabled', 'true')
+    
+    let interval = setInterval(function() {
+        let time = parseFloat($time.textContent)
+        
+        if (time <= 0) {
+            clearInterval(interval)
+            gameStop()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
+
     renderBox()
 }
 
-document.addEventListener('click', handleBoxClick)
+function gameStop() {
+    isGameStarted = false
+    $game.innerHTML = ''
+    $game.style.backgroundColor = '#ccc'
+    $start.classList.remove('hide')
+    $handelTime.classList.add('hide')
+    $handleScore.classList.remove('hide')
+    $gameTime.removeAttribute('disabled')
+    setGameScore()
+}
+
+function setGameScore() {
+    $result.textContent = score.toString()
+}
+
+function setGameTime() {
+    $handelTime.classList.remove('hide')
+    $handleScore.classList.add('hide')
+    let time = +$gameTime.value
+    $time.textContent = time + '.0'
+}
+
 
 function handleBoxClick(e) {
     if (e.target.dataset.box) {
