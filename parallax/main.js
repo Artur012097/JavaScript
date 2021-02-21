0da88f74081974e9e0af31e4f22e0b78c6de1c8e
artur012097 <artur012097.ax@gmail.com>
2021-02-21 19:28:59 +0400

    Поправил моладежные неисправности :) .

--- parallax/main.js
+++ parallax/main.js
@@ -1,14 +1,21 @@
-
+let first = document.querySelector('.first');
+    h1 = document.querySelector('h1');
+first.style.transform = 'scale(1)'
 
 window.addEventListener('scroll', () => {
-    let first = document.querySelector('.first');
     let second = document.querySelector('.second');
     let third = document.querySelector('.third');
-    let h1 = document.querySelector('h1');
     let y = window.scrollY;
 
+    if (y == 0) {
+        first.style.transform = 'scale(1)'
+        h1.style.top = '80%';
+        h1.style.left = '20%'
+    } else {
+        first.style.transform = 'scale(' + (1 + y/1000)+  ')'
+        h1.style.top = (80 - y/5) + '%';
+        h1.style.left = (20 + y/50) + '%'
+    }
     second.style.transform = 'translate(' + y/5 + 'px' + ',' + y/3 + 'px' + ')';
     third.style.transform = 'translate(' + -y/5 + 'px' + ',' + y/3 + 'px' + ')';
-    h1.style.top = `100%`;
-    h1.style.top = '1' + y/100 + '%';
 })
