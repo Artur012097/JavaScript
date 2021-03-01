4760b2f598cff24a5fa697d0d56be70713ed6159
artur012097 <artur012097.ax@gmail.com>
2021-03-01 16:41:28 +0400

    Learn webpack dev server, babel, less, sass

--- lesson-WebPack/src/index.js
+++ lesson-WebPack/src/index.js
@@ -2,6 +2,7 @@ import {config} from './modules/config'
 import App from './modules/app.component'
 import './modules/header.component'
 import './css/index.css'
+import './scss/index.scss'
 
 let app = new App('Hello')
 app.log()
--- lesson-WebPack/webpack.config.js
+++ lesson-WebPack/webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        minimizer: [
+          new CssMinimizerPlugin({}),
+          new TerserPlugin({})
+        ],
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: 4200
     },
     plugins: [
         new HTMLPlugin({
@@ -23,6 +34,24 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.less$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                }
             }
         ],
     },
