f548ec478eb331a1dde95790f5c42aba6693cdec
artur012097 <artur012097.ax@gmail.com>
2021-03-02 11:34:03 +0400

    Lesson end

--- lesson-WebPack/src/index.js
+++ lesson-WebPack/src/index.js
@@ -3,8 +3,13 @@ import App from './modules/app.component'
 import './modules/header.component'
 import './css/index.css'
 import './scss/index.scss'
+import axios from 'axios'
 
 let app = new App('Hello')
 app.log()
 
 console.log(config.item)
+
+axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.data)
+    .then((data) => {console.log(data)})
--- lesson-WebPack/webpack.config.js
+++ lesson-WebPack/webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin")
+const axios = require('axios').default;
 
 module.exports = {
     entry: './src/index.js',
